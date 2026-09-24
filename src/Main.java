4cd62a94da52f90a830eaf403e07399454b9f262
Anne-YP <anpe1015@stud.ek.dk>
2026-09-24 13:01:41 +0200

    Added main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome!");
+        System.out.println("You are trapped in a labyrinth and you need to find your way out. To grasp your whereabouts type in 'look'");
+
+        Adventure adventure = new Adventure();
+        adventure.play(); // Start/Slut spillet
+    }
+
+}
