8bfe763204e2af40b1636d388efe945d0291b062
idabloust <idbl1000@stud.ek.dk>
2026-09-24 14:13:54 +0200

    Room4

--- src/Adventure.java
+++ src/Adventure.java
@@ -13,6 +13,9 @@ public class Adventure {
         Room r3 = new Room("You are in room 3",
                 "There is a tree growing in a ray of light. A sword is hanging from one of its branches.");
 
+        Room r4 = new Room("You are in room 4",
+                "There is a orc standing in front of you. What do you do?")
+
         // Room 1 setters
         r1.setEast(r2);
 
